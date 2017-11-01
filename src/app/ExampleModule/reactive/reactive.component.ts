9573b82d8523b04fb0d518fd46567f4f7064d908
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-11-01 11:54:10 +0500

    added validation to reactive form

--- src/app/ExampleModule/reactive/reactive.component.ts
+++ src/app/ExampleModule/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 // import { ExampleModel } from "./example.model";
 @Component({
@@ -9,13 +9,15 @@ import { FormControl, FormGroup } from "@angular/forms";
 export class ReactiveComponent implements OnInit {
   constructor() {}
   user: FormGroup;
-
+  onSubmit() {
+    console.log(this.user.value, this.user.valid);
+  }
   ngOnInit() {
     this.user = new FormGroup({
-      name: new FormControl(""),
+      name: new FormControl("", [Validators.required, Validators.minLength(2)]),
       account: new FormGroup({
-        email: new FormControl(""),
-        confirm: new FormControl("")
+        email: new FormControl("", Validators.required),
+        confirm: new FormControl("", Validators.required)
       })
     });
   }

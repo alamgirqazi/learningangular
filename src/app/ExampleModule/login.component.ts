2649ef8be0ccd089d98b741dc58f1ac6a2000a09
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-11-08 15:52:58 +0500

    testing modal and dropdown of ngbootstrap

--- src/app/ExampleModule/login.component.ts
+++ src/app/ExampleModule/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { AuthService } from "./../projectscomponent/auth.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { AuthHttp } from "angular2-jwt";
 
 interface Credentials {
   username: string;
@@ -10,33 +11,81 @@ interface Credentials {
 
 @Component({
   selector: "app-login",
+  styles: [
+    `.submitted {
+  border: 1px solid red;}
+  .submittedgreen {
+    border: 1px solid green;}`
+  ],
   template: `
- <h2 *ngIf="auth.loggedIn()">Wow you're logged in</h2> 
-    <form #f="ngForm" (ngSubmit)="onLogin(f.value)"  *ngIf="!auth.loggedIn()">
+<div class="row">
+<div class="col-sm-6">
+    <form #f="ngForm" (ngSubmit)="onLogin(f.value)">
     <br>
     <br>
-      <input type="text" placeholder="username" #username="ngModel" class="form-control" required name="username" ngModel ngControl="username">
+      <input type="text" (change)="changeVal(username)" [class.submitted]="isUserError"  placeholder="username" #username="ngModel" class="form-control" required name="username" ngModel ngControl="username">
       <br>
       <div *ngIf="f.submitted && !username.valid" class="alert alert-danger">
       <!-- <div [hidden]="!heroForm.submitted || (name.valid || name.pristine)" class="alert alert-danger"> -->
       Name is required
     </div>
-      <input type="password" placeholder="password" #password="ngModel" name="password" minlength="3" maxLength="24" required  class="form-control" ngModel ngControl="password">
+   
+      <input [class.submitted]="isPassError" (change)="changePassVal(password)" type="password" placeholder="password" #password="ngModel" name="password" minlength="3" maxLength="24" required  class="form-control" ngModel ngControl="password">
       <br>
       <div *ngIf="f.submitted && !password.valid" class="alert alert-danger">
       <!-- <div [hidden]="!heroForm.submitted || (name.valid || name.pristine)" class="alert alert-danger"> -->
-      Choose password between 8 to 24 characters
+      Choose password between 3 to 24 characters
     </div>
+    <br>
+    <div *ngIf="isError" class="alert alert-danger">
+    <!-- <div [hidden]="!heroForm.submitted || (name.valid || name.pristine)" class="alert alert-danger"> -->
+    {{errorText}}
+  </div>
+  <br>
       <button type="submit">Submit</button>
-    
+      <br>
+      <br>
+      <br>
+      
+      <div *ngIf="isSuccess" class="alert alert-success">
+      <!-- <div [hidden]="!heroForm.submitted || (name.valid || name.pristine)" class="alert alert-danger"> -->
+      User Logged in successfully
+    </div>
       </form>
+      </div>
+      </div>
   `
 })
 export class LoginComponent {
   credentials: Credentials;
-
+  isError: boolean;
+  isSuccess: boolean;
+  isPassError: boolean;
+  isUserError: boolean;
+  errorText = "";
   constructor(private auth: AuthService, private http: HttpClient) {}
+//   showError(): boolean
+// {if(this.isError === true && username.valid)
+//  return true;
+//  else return false
 
+// }
+changeVal(username)
+{
+  console.log("value changed")
+  console.log(username);
+if(username.valid)
+this.isUserError = false;
+
+}
+changePassVal(password)
+{
+  console.log("value changed")
+  console.log(password);
+if(password.valid)
+this.isPassError = false;
+
+}
   onLogin(credentials) {
     // let url = "http://localhost:3000/users/login"
 
@@ -46,19 +95,36 @@ export class LoginComponent {
         {
           username: credentials.username,
           password: credentials.password
-        },
-        {
-          headers: new HttpHeaders().set("Authorization", "my-auth-token")
         }
+        // ,
+        // {
+        //   headers: new HttpHeaders().set("Authorization", "my-auth-token")
+        // }
       )
-      .subscribe(data => {
-        // console.log("tokenKey", data["id"]);
-        console.log(data);
-        localStorage.setItem("id_token", JSON.stringify(data));
-        localStorage.setItem("id", data["id"]);
-        localStorage.setItem("ttl", data["ttl"]);
-        localStorage.setItem("userId", data["userId"]);
-      });
+      .subscribe(
+        data => {
+          // console.log("tokenKey", data["id"]);
+          console.log(data);
+          localStorage.setItem("id_token", JSON.stringify(data));
+          localStorage.setItem("id", data["id"]);
+          localStorage.setItem("ttl", data["ttl"]);
+          localStorage.setItem("userId", data["userId"]);
+          this.isError = false;
+          this.isSuccess = true;
+          this.isUserError = false;
+          this.isPassError = false;
+        },
+        err => {
+          console.log("Something went wrong!");
+          console.log(err);
+          this.isError = true;
+          this.errorText = "Error! Wrong credentials"; 
+               this.isSuccess = false;
+          this.isUserError = true;
+          this.isPassError = true;
+        }
+      );
+
     this.auth.login(credentials);
   }
 }
--- src/app/lazymodule/morelazy.component.ts
+++ src/app/lazymodule/morelazy.component.ts
@@ -1,7 +1,32 @@
 import { Component } from "@angular/core";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  template: `<h3>More Component</h3>
-  `
+  templateUrl: "./morelazy.component.html"
 })
-export class MoreLazyComponent {}
+export class MoreLazyComponent {
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content).result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+}

486995c610dc27d02b7551949849b5a5fb147083
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-11-09 11:29:22 +0500

    added @output and eventemitter

--- src/app/lazymodule/clazy.component.ts
+++ src/app/lazymodule/clazy.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "lazy-clazy",
 
   template: `<h3>Child Lazy</h3>
   <p>{{childMessage}}</p>
+  <button (click)="sendMessage()">Send Message</button>
+  
   `
 })
 export class ClazyComponent {
   @Input() childMessage: string;
+
+  message: string = "Hola Mundo!";
+  @Output() messageEvent = new EventEmitter<string>();
+  constructor() {}
+  sendMessage() {
+    this.messageEvent.emit(this.message);
+  }
 }
--- src/app/lazymodule/lazy.component.ts
+++ src/app/lazymodule/lazy.component.ts
@@ -6,12 +6,18 @@ import { Router } from "@angular/router";
  <button (click)="seeMore()"> Goto More </button>
  <button (click)="childlazy()"> Child Comp </button>
 <lazy-clazy [childMessage]="parentMessage"></lazy-clazy>
+
+<h1> New MSG from @output: {{message}}</h1>
+<lazy-clazy (messageEvent)="receiveMessage($event)"></lazy-clazy>
   `
 })
 export class LazyComponent {
   constructor(public router: Router) {}
   parentMessage = "message from parent";
-
+  message: string;
+  receiveMessage($event) {
+    this.message = $event;
+  }
   seeMore() {
     this.router.navigate(["/lazy/more"]);
   }

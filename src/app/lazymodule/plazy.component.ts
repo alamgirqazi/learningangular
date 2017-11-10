7f72a3d4358e0712fa4a100888437a5e414b39f4
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-11-10 15:45:43 +0500

    added delete and update in ngfor list items

--- src/app/lazymodule/plazy.component.ts
+++ src/app/lazymodule/plazy.component.ts
@@ -7,7 +7,10 @@ import { DisplayData } from "./../projectscomponent/data.service";
 <ul>
 <li *ngFor="let res of results; trackBy: trackById;let i = index;"> 
   {{ res.body }}
-  <br> <span (click)="delete(i)">X</span>
+  <br> <button (click)="delete(i)">X</button>
+  <br><br> <button (click)="update(i)">Update</button>
+  <br>
+  <br>
 </li>
 </ul>
   `
@@ -32,6 +35,9 @@ export class PlazyComponent {
     this.results.splice(index, 1);
     console.log(this.results);
     // alert(JSON.stringify(res));
+  }
+  update(index) {
+    this.results[Object.keys(this.results)[index]].body = "Updated";
   }
   async getResults() {
     this.results = await this.data.getPosts();

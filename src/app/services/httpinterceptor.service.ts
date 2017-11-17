51749b84ac76dfad8c014043590ee2bbe1f6a4a2
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2017-11-17 17:47:45 +0500

    removed http interceptor service for now

--- src/app/services/httpinterceptor.service.ts
+++ src/app/services/httpinterceptor.service.ts
@@ -1,36 +1,68 @@
-import { Injectable } from "@angular/core";
-import {
-  HttpEvent,
-  HttpInterceptor,
-  HttpHandler,
-  HttpRequest
-} from "@angular/common/http";
-import { Auth0Service } from "./auth.service";
-import { Observable } from "rxjs/Observable";
-import { Injector } from "@angular/core";
+// // import { Injectable } from "@angular/core";
+// // import {
+// //   HttpEvent,
+// //   HttpInterceptor,
+// //   HttpHandler,
+// //   HttpRequest
+// // } from "@angular/common/http";
+// // import { Auth0Service } from "./auth.service";
+// // import { Observable } from "rxjs/Observable";
+// // import { Injector } from "@angular/core";
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  private authService: Auth0Service;
+// // @Injectable()
+// // export class AuthInterceptor implements HttpInterceptor {
+// //   private authService: Auth0Service;
 
-  constructor(private injector: Injector) {}
+// //   constructor(private injector: Injector) {}
 
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    // Get the auth header from the service.
-    this.authService = this.injector.get(Auth0Service); // get it here within intercept
+// //   intercept(
+// //     req: HttpRequest<any>,
+// //     next: HttpHandler
+// //   ): Observable<HttpEvent<any>> {
+// //     // Get the auth header from the service.
+// //     this.authService = this.injector.get(Auth0Service); // get it here within intercept
 
-    // const authHeader = this.authService.getAccessTokenId();
-    // Clone the request to add the new header.
-    const authReq = req.clone({
-      headers: req.headers.set(
-        "Authorization",
-        this.authService.getAccessTokenId()
-      )
-    });
-    // Pass on the cloned request instead of the original request.
-    return next.handle(authReq);
-  }
-}
+// //     // const authHeader = this.authService.getAccessTokenId();
+// //     // Clone the request to add the new header.
+// //     const authReq = req.clone({
+// //       headers: req.headers.set(
+// //         "Authorization",
+// //         this.authService.getAccessTokenId()
+// //       )
+// //     });
+// //     // Pass on the cloned request instead of the original request.
+// //     return next.handle(authReq);
+// //   }
+// // }
+
+// import { Injector } from "@angular/core";
+
+// import { Injectable } from "@angular/core";
+// import {
+//   HttpRequest,
+//   HttpHandler,
+//   HttpEvent,
+//   HttpInterceptor
+// } from "@angular/common/http";
+// // import { AuthService } from "./auth/auth.service";
+// import { Observable } from "rxjs/Observable";
+// import { Auth0Service } from "./auth.service";
+// @Injectable()
+// export class TokenInterceptor implements HttpInterceptor {
+//   constructor(private injector: Injector) {}
+//   private auth0: Auth0Service;
+
+//   intercept(
+//     request: HttpRequest<any>,
+//     next: HttpHandler
+//   ): Observable<HttpEvent<any>> {
+//     this.auth0 = this.injector.get(Auth0Service); // get it here within intercept
+
+//     request = request.clone({
+//       setHeaders: {
+//         Authorization: `Bearer ${this.auth0.getAccessTokenId()}`
+//       }
+//     });
+//     return next.handle(request);
+//   }
+// }
